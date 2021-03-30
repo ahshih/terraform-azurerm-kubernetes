b89bb3d85a1b910efbb920368280d82ab2fd1966
Albert Shih <Albert.Shih@neudesic.com>
2021-03-30 14:45:30 -0700

    Additional fix for principal vs identity ids

--- main.tf
+++ main.tf
@@ -151,6 +151,6 @@ resource "azurerm_role_assignment" "acr_pull" {
   for_each                         = var.acr_pull_access
   scope                            = each.value
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.principal_id
   skip_service_principal_aad_check = true
 }
